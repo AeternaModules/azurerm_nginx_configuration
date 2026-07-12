ca3ee0c6ab8290525ea3225a2ca79be00098a374
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,22 @@
-output "nginx_configurations" {
-  description = "All nginx_configuration resources"
-  value       = azurerm_nginx_configuration.nginx_configurations
-  sensitive   = true
-}
 output "nginx_configurations_config_file" {
-  description = "List of config_file values across all nginx_configurations"
-  value       = [for k, v in azurerm_nginx_configuration.nginx_configurations : v.config_file]
+  description = "Map of config_file values across all nginx_configurations, keyed the same as var.nginx_configurations"
+  value       = { for k, v in azurerm_nginx_configuration.nginx_configurations : k => v.config_file }
 }
 output "nginx_configurations_nginx_deployment_id" {
-  description = "List of nginx_deployment_id values across all nginx_configurations"
-  value       = [for k, v in azurerm_nginx_configuration.nginx_configurations : v.nginx_deployment_id]
+  description = "Map of nginx_deployment_id values across all nginx_configurations, keyed the same as var.nginx_configurations"
+  value       = { for k, v in azurerm_nginx_configuration.nginx_configurations : k => v.nginx_deployment_id }
 }
 output "nginx_configurations_package_data" {
-  description = "List of package_data values across all nginx_configurations"
-  value       = [for k, v in azurerm_nginx_configuration.nginx_configurations : v.package_data]
+  description = "Map of package_data values across all nginx_configurations, keyed the same as var.nginx_configurations"
+  value       = { for k, v in azurerm_nginx_configuration.nginx_configurations : k => v.package_data }
 }
 output "nginx_configurations_protected_file" {
-  description = "List of protected_file values across all nginx_configurations"
-  value       = [for k, v in azurerm_nginx_configuration.nginx_configurations : v.protected_file]
+  description = "Map of protected_file values across all nginx_configurations, keyed the same as var.nginx_configurations"
+  value       = { for k, v in azurerm_nginx_configuration.nginx_configurations : k => v.protected_file }
   sensitive   = true
 }
 output "nginx_configurations_root_file" {
-  description = "List of root_file values across all nginx_configurations"
-  value       = [for k, v in azurerm_nginx_configuration.nginx_configurations : v.root_file]
+  description = "Map of root_file values across all nginx_configurations, keyed the same as var.nginx_configurations"
+  value       = { for k, v in azurerm_nginx_configuration.nginx_configurations : k => v.root_file }
 }
 
